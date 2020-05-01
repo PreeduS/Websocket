34d9f99052511bf10e6fb2b31e8e222a6b1965cc
preedus <preedus@gmail.com>
2020-05-01 19:19:11 +0300

    Fix frontend build

--- fe/docker-entrypoint.sh
+++ fe/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-API_HOST=${API_HOST:-localhost}
-API_PORT=${API_PORT:-3000}
+export API_HOST=${API_HOST:-http://localhost}
+export API_PORT=${API_PORT:-3000}
 
 envsubst '${API_HOST} ${API_PORT}' < /tmp/nginx.conf.template > /etc/nginx/nginx.conf
 

1b85073b1604c95221410d51beea107f89ad1672
PreeduS <preedus@gmail.com>
2020-05-17 15:29:20 +0300

    Update ws prod endpoint

--- fe/app/src/environments/environment.prod.ts
+++ fe/app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   endpoint: `${window.location.origin}/api`,
-  endpointWS: `${window.location.origin}/ws`,
+  endpointWS: `ws://${window.location.host}/ws`,
 };
 

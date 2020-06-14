2c757d169a70e259fe7594c24b64c09dd6a8b0a9
PreeduS <preedus@gmail.com>
2020-06-14 23:05:13 +0300

    Add frontend nginx port env var

--- fe/docker-entrypoint.sh
+++ fe/docker-entrypoint.sh
@@ -3,8 +3,9 @@ set -eu
 
 export API_HOST=${API_HOST:-http://localhost}
 export API_PORT=${API_PORT:-3000}
+export PORT=${PORT:-80}
 
-envsubst '${API_HOST} ${API_PORT}' < /tmp/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${PORT} ${API_HOST} ${API_PORT}' < /tmp/nginx.conf.template > /etc/nginx/nginx.conf
 
 
 exec "$@"
